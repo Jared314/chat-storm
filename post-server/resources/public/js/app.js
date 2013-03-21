0f347afa8f137410f24da61a4ba7893500ce641a
Jared Lobberecht <pretender314@gmail.com>
2013-03-21 01:33:51 -0500

    fixing web ui ports and adding initial state fetch

--- post-server/resources/public/js/app.js
+++ post-server/resources/public/js/app.js
@@ -1,21 +1,28 @@
 $(function() {
-  window.socketurl = window.location.href.replace("http://", "ws://");
-                                         //.replace(':5000', ':5001');
-  //console.log(window.socketurl);
-  window.socket = new WebSocket(socketurl);
+  window.socketurl = 'ws://' + window.location.hostname + ':81/';
+  window.stateurl = '//' + window.location.hostname + ':82/';
+  window.username = 'user' + Math.floor((Math.random()*1000000)+1000);
 
+  // Show random username
+  $("#username").text(window.username);
+
+  // State update socket
+  window.socket = new WebSocket(socketurl);
   socket.onmessage = function(msg) {
     $("#messages").append("<p>" + msg.data + "</p>");
   };
-
-  window.username = 'user' + Math.floor((Math.random()*1000000)+1000);
-  $("#username").text(window.username);
-
   $("#socket").on("submit", function(e) {
     e.preventDefault();
-    //socket.send(window.username + ': ' + $("#message").val());
     $.post('', window.username + ': ' + $("#message").val(), function(){
       $("#message").val("");
+    });
+  });
+
+  // Get initial state
+  $.getJSON(window.stateurl, function(data, textStatus, jqXHR){
+    console.log(data);
+    $.each(data.data, function() {
+      $("#messages").append("<p>" + this + "</p>");
     });
   });
 
