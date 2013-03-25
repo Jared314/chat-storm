f73ca9f7ea84c490a313fb89ae1431bff8385b0b
Jared Lobberecht <pretender314@gmail.com>
2013-03-25 04:17:23 -0500

    switching to redis for data store

--- status-server/project.clj
+++ status-server/project.clj
@@ -4,7 +4,7 @@
                  [compojure "1.1.5"]
                  [ring "1.1.8"]
                  [org.clojure/data.json "0.2.1"]
-                 [clojurewerkz/spyglass "1.1.0-beta3"]]
+                 [com.taoensso/carmine "1.7.0-beta2"]]
   :aot [status-server.core]
   :main status-server.core
   :min-lein-version "2.0.0")
--- status-server/src/status_server/core.clj
+++ status-server/src/status_server/core.clj
@@ -7,27 +7,29 @@
               [ring.middleware.resource :refer :all]
               [ring.util.response :as response]
               [clojure.data.json :as json]
-              [clojurewerkz.spyglass.client :as memcache])
+              [taoensso.carmine :as car])
     (:gen-class))
 
 (def cache-connection (ref nil))
 (def cache-key "room1")
+(def pool (car/make-conn-pool))
+(def spec-server1 (car/make-conn-spec :host "127.0.0.1" :port 6379))
 
 (def options-response 
      {:status 200 
       :headers {"Access-Control-Allow-Origin" "*"
                 "Access-Control-Allow-Methods" "GET, OPTIONS"}})
 
-(defn get-memcached-status []
-      (let [data (memcache/get @cache-connection cache-key)]
-               {:data (or data [])}))
+(defn get-cached-status []
+      (let [data (car/with-conn pool spec-server1 (car/lrange cache-key 0 -1))]
+                 {:data data}))
 
 (defn get-status [request]
       {:status 200
        :headers {"content-type" "application/json"
                  "Access-Control-Allow-Origin" "*"
                  "Access-Control-Allow-Methods" "GET, OPTIONS"}
-       :body (json/write-str (get-memcached-status))})
+       :body (json/write-str (get-cached-status))})
 
 (defroutes app-routes
            (GET ["/"] {} get-status)
@@ -39,6 +41,6 @@
 
 (defn -main [& options]
   (let [port (Integer. (nth options 0 5000))
-        memcache-ip (nth options 1 "127.0.0.1:11211")]
-       (dosync (ref-set cache-connection (memcache/text-connection memcache-ip)))
+        cache-ip (nth options 1 "127.0.0.1:6379")]
+       ;(dosync (ref-set cache-connection (memcache/text-connection cache-ip)))
        (run-jetty app {:port port})))
