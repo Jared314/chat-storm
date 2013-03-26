5984acff4e80e35789d613140e602dc642656c42
Jared Lobberecht <pretender314@gmail.com>
2013-03-26 04:33:42 -0500

    fixing version1 topology not deploying correctly

--- storm-topology/src/storm_topology/version1.clj
+++ storm-topology/src/storm_topology/version1.clj
@@ -26,7 +26,7 @@
 (defn build-topology [queuehost queuename cachekey cachehost cachelimit posthost]
       (storm/topology {"messages" (storm/spout-spec (KestrelThriftSpout. queuehost 2229 queuename (StringScheme.))
                                                     :p 2)}
-                      {"broadcaster" (storm/bolt-spec {"messages" :localOrShuffle}
+                      {"broadcaster" (storm/bolt-spec {"messages" :shuffle}
                                                       (broadcaster cachekey cachehost cachelimit posthost)
                                                       :p 2)}))
 
@@ -37,6 +37,6 @@
             cachehost "ip-10-248-44-154.us-west-2.compute.internal"
             cachelimit (dec 50)
             posthost "http://ip-10-248-44-154.us-west-2.compute.internal:81"]
-           (helpers/bootstrap (build-topology queuehost queuename) 
-                              name 
+           (helpers/bootstrap (build-topology queuehost queuename cachekey cachehost cachelimit posthost)
+                              (first name)
                               {storm-config/TOPOLOGY-WORKERS 2})))
