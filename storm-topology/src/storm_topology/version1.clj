01dfeeda95797e269909c2ef1d55910c09f33377
Jared Lobberecht <pretender314@gmail.com>
2013-03-26 02:57:19 -0500

    parameterizing the bolt's connection info

--- storm-topology/src/storm_topology/version1.clj
+++ storm-topology/src/storm_topology/version1.clj
@@ -8,32 +8,35 @@
              [backtype.storm.scheme StringScheme])
     (:gen-class))
 
-(def queuename "room1")
-(def queuehost "ip-10-248-44-154.us-west-2.compute.internal")
-(def cachekey "room1")
-(def cachehost "ip-10-248-44-154.us-west-2.compute.internal")
-(def cachelimit (dec 50))
-(def posthost "http://ip-10-248-44-154.us-west-2.compute.internal:81")
+(storm/defbolt broadcaster [] 
+               {:prepare true :params [cachekey cachehost cachelimit posthost]} 
+               [conf context collector]
+               (let [pool (redis/make-conn-pool)
+                     spec-server1 (redis/make-conn-spec :host cachehost :port 6379)]
+                    (storm/bolt-execute [tuple]
+                      (let [message (.getStringByField tuple "str")]
+                           (http/post posthost {:body message})
+                           (redis/with-conn pool spec-server1
+                                            (redis/multi)
+                                            (redis/lpush cachekey message)
+                                            (redis/ltrim cachekey 0 cachelimit)
+                                            (redis/exec))
+                           (storm/ack! collector tuple)))))
 
-(def pool         (redis/make-conn-pool))
-(def spec-server1 (redis/make-conn-spec :host cachehost :port 6379))
-
-(storm/defbolt broadcaster [] [tuple collector]
-         (let [message (.getStringByField tuple "str")]
-              (http/post posthost {:body message})
-              (redis/with-conn pool spec-server1
-                             (redis/multi)
-                             (redis/lpush cachekey message)
-                             (redis/ltrim cachekey 0 cachelimit)
-                             (redis/exec))
-              (storm/ack! collector tuple)))
-
-(defn build-topology []
-      (storm/topology {"message source" (storm/spout-spec (KestrelThriftSpout. queuehost 2229 queuename (StringScheme.))
-                                                          :p 2)}
-                      {"broadcaster" (storm/bolt-spec {"message source" :shuffle}
-                                                      broadcaster
+(defn build-topology [queuehost queuename cachekey cachehost cachelimit posthost]
+      (storm/topology {"messages" (storm/spout-spec (KestrelThriftSpout. queuehost 2229 queuename (StringScheme.))
+                                                    :p 2)}
+                      {"broadcaster" (storm/bolt-spec {"messages" :localOrShuffle}
+                                                      (broadcaster cachekey cachehost cachelimit posthost)
                                                       :p 2)}))
 
-(defn -main [& name] 
-      (helpers/bootstrap (build-topology) name {storm-config/TOPOLOGY-WORKERS 2}))
+(defn -main [& name]
+      (let [queuehost "ip-10-248-44-154.us-west-2.compute.internal"
+            queuename "room1"
+            cachekey "room1"
+            cachehost "ip-10-248-44-154.us-west-2.compute.internal"
+            cachelimit (dec 50)
+            posthost "http://ip-10-248-44-154.us-west-2.compute.internal:81"]
+           (helpers/bootstrap (build-topology queuehost queuename) 
+                              name 
+                              {storm-config/TOPOLOGY-WORKERS 2})))
