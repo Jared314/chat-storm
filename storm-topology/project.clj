5fc8e00b3e91cf15a881854fe5a08321635d216a
Jared Lobberecht <pretender314@gmail.com>
2013-03-28 15:50:30 -0500

    updating http dependency

--- storm-topology/project.clj
+++ storm-topology/project.clj
@@ -1,6 +1,6 @@
 (defproject storm-topology "0.0.1-SNAPSHOT"
   :dependencies [[storm/storm-kestrel "0.7.2-SNAPSHOT"]
-                 [clj-http "0.6.5"]
+                 [clj-http "0.7.0"]
                  [com.taoensso/carmine "1.7.0-beta2"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]
                                   [storm "0.8.2"]]}}
